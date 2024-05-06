fa87b5b0a24ad462e12e91335f6f868e0f703ea0
dhrutisubham <dhruti.subham@gmail.com>
2024-05-06 08:42:40 +0530

    git init

---
+++ components/question/Question.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Question = () => {
+  return (
+    <div>question</div>
+  )
+}
+
+export default Question
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Question from '../components/question/Question'
+
+function App() {
+  
+
+  return (
+    <>
+      <Question></Question>
+    </>
+  )
+}
+
+export default App
