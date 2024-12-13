9433533316ba9144c1c3b6ba3bdaf9cb7889cf8b
dhrutisubham <dhruti.subham@gmail.com>
2024-12-13 23:11:55 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'
-import Question from './components/question/question'
+import Question from './components/question/question.jsx'
 
 
 function App() {
